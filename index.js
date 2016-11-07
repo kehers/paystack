3560738db0f862f4afbb5f97101d771d5d659d2d
Subomi <subomioluwalana71@gmail.com>
2016-11-07 15:34:43 +0100

    removed l, and tests still pass

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ Paystack.prototype = {
      
       if (argsInEndpoint) {
         l = argsInEndpoint.length;
-        l
         // Do we have one or more?
         if (l > 0) {
           // Confirm resource declaration good
