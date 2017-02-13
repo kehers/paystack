3b35bebc75911eda6fe6193e68f1025cbb8c42b7
Subomi <subomioluwalana71@gmail.com>
2017-02-13 03:38:56 +0100

    removed promised bugs

--- test/misc.js
+++ test/misc.js
@@ -7,28 +7,26 @@ describe("Paystack Miscellanous Functions", function() {
 
   // List Banks
   it("should list all supported banks", function(done) {
-    paystack.misc.list_banks()
-      .then(function(body){
-        expect(body).to.have.property('data');
-        expect(body.data[0]).to.have.property('name');
-        done();
-      })
-      .catch(function(error){
-        return done(error);
-      });
+    paystack.misc.list_banks(function(error, body){
+      if(error) 
+      	return done(error);
+    
+      expect(body).to.have.property('data');
+      expect(body.data[0]).to.have.property('name');
+      done();
+    })
   });
   
   // Resolve a Bin Card
   it("should resolve a bin card", function(done) {
-    paystack.misc.resolve_bin(59983)
-      .then(function(body){
-        expect(body).to.have.property('data');
-        expect(body.data).to.have.property('bin');
-        done();
+    paystack.misc.resolve_bin(59983, function(error, body){
+      if(error) 
+        return done(error);    
+        
+      expect(body).to.have.property('data');
+      expect(body.data).to.have.property('bin');
+      done();
       })
-      .catch(function(error){
-        return done(error);
-      });
   });
 
 });
